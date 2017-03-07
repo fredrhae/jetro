4c558b40868d276add673724aaca272aacd36153
Frederico Rhae <fredrhae@gmail.com>
2017-03-07 00:02:30 -0300

    Retirada de várias dependências da tela de login
    Foi feita uma tela de login mais simples, sem as dezenas
    de dependências. Achei que ia ficar uma tela muito carregada, e preferi
    usar um template mais simples de login com bootstrap.

--- src/main/java/com/jetro/security/SecurityConfig.java
+++ src/main/java/com/jetro/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -33,10 +34,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(new String[] {"/global/**", "/assets/**","/images/**", "/login"}).permitAll()
 			.anyRequest().authenticated()
 			.and()
-		.formLogin()
-			.loginPage("/login")
-			.and()
-		.httpBasic();
+			.formLogin()
+				.loginPage("/login")
+				.and()
+            .logout() 
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) 
+                .logoutSuccessUrl("/") 
+                .permitAll()
+                .and()
+            .httpBasic();
+
 		
 	}
 }
